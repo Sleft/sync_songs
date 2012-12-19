0b8448936a56927fb75bb52e6683f8d988bdc475
Sleft <fte08eas@student.lu.se>
2012-12-19 20:02:06 +0100

    First commit

---
+++ lib/sync-fav-songs/song.rb
@@ -0,0 +1,28 @@
+module SyncFavSongs
+  ##
+  # Stores a song
+  class Song
+    attr_reader :artist, :title
+
+    ##
+    # Constructs a song of the given artist and title.
+    def initialize(artist, title)
+      @artist = artist
+      @title  = title
+    end
+
+    ##
+    # Two songs are equal if they have the same title and are by the
+    # same independently of the case of either.
+    def eql?(song)
+      title.casecmp(song.title) == 0 and
+        artist.casecmp(song.artist) == 0
+    end
+
+    ##
+    # Returns the song conventionally formatted.
+    def to_s
+      "#{artist} - #{title}"
+    end
+  end
+end
---
+++ lib/sync-fav-songs/song_list.rb
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+require 'set'
+# require_relative 'song'
+require './song'
+
+module SyncFavSongs
+  ##
+  # Stores a list of songs.
+  class SongList
+    include Enumerable          # For +include?+
+
+    ##
+    # Constructs a new song list.
+    def initialize
+      @songs = Set.new
+    end
+
+    ##
+    # Adds a song to the end of the list.
+    def add(song)
+      @songs.add(song)
+      self                     # To enable chaining of calls to +add+
+    end
+
+    ##
+    # Calls _block_ once for each element in self, passing that
+    # element as a parameter. Implemented mostly for enabling the
+    # +Enumerable+ mixin.
+    def each(&block)
+      @songs.each(&block)
+    end
+
+    ##
+    # Returns songs that are in this list but not the given list.
+    #--
+    # TODO: Reducera till array och fråga varför clone och dup-tjafset
+    # inte fungerar
+    def exclusive(compared_list)
+      # # Select those songs that
+      # @songs.select do |song|
+      #   # are such that they are not in the compared list
+      #   compared_list.none? { |compared_song| song.eql?(compared_song) }
+      # end
+
+      @songs ^ compared_list
+    end
+  end
+
+  # Gör riktigt test av detta! Se pickaxe
+  song1 = Song.new("1", "")
+  song2 = Song.new("2", "")
+  song3 = Song.new("3", "")
+  song4 = Song.new("4", "")
+  song5 = Song.new("5", "")
+  song6 = Song.new("6", "")
+
+  # puts song1.eql?(song2)
+  # puts song1.eql?(song3)
+  # puts song2.eql?(song3)
+
+  list1 = SongList.new
+  list1.add(song1).add(song3).add(song5).add(song1)
+  list2 = SongList.new
+  list2.add(song1).add(song2).add(song3).add(song4).add(song6)
+
+  puts list2.exclusive(list1).to_a
+end
