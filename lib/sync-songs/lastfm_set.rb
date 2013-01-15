15ac87161c8f8f3c6860a1a41f87af898f566d7d
Sleft <fte08eas@student.lu.se>
2013-01-15 17:23:22 +0100

    LastfmSet should have the same methods as other subclasses of SongList. LastfmSet uses highline rather than gets.

--- lib/sync-songs/lastfm_set.rb
+++ lib/sync-songs/lastfm_set.rb
@@ -1,38 +1,47 @@
 # -*- coding: utf-8 -*-
 
 require 'lastfm'
-require 'launchy'
+require 'highline/import'       # Should be in UI?
+require 'launchy'               # Should be in UI?
 require_relative 'song_set'
 
 # Public: Classes for syncing sets of songs.
 module SyncSongs
   # Public: A set of Grooveshark songs.
   class LastfmSet < SongSet
+    @@DEFAULT_LIMIT = 1_000_000
 
     # Public: Constructs a Last.fm set by logging in to
     # Last.fm with the specified user.
     #
     # username - The username of the user to authenticate.
     # password - The password of the user to authenticate.
-    def initialize(api_key, api_secret)
+    # limit    - The maximum number of results from calls (default:
+    #            @@DEFAULT_LIMIT).
+    def initialize(api_key, api_secret, limit = @@DEFAULT_LIMIT)
       super()
       @api_key = api_key
       @lastfm = Lastfm.new(api_key, api_secret)
+      @limit = limit
     end
 
     # Public: Get the user's loved songs from Last.fm.
     #
     # username - The username of the user to authenticate.
-    # limit    - The maximum number of favorites to get.
+    # 
+    # limit    - The maximum number of favorites to get (default:
+    #            @limit).
     #
     # Raises Lastfm::ApiError if the username is invalid.
-    def getLoved(username, limit)
+    def getLoved(username, limit = @limit)
       @lastfm.user.get_loved_tracks(user: username,
                                     api_key: @api_key,
                                     limit: limit).each do |s|
         add(Song.new(s['name'], s['artist']['name']))
       end
     end
+
+    alias_method :getFavorites, :getLoved
 
     # Public: Add the songs in the given set to the given user's
     # loved songs on Last.fm.
@@ -45,21 +54,28 @@ module SyncSongs
     # Returns an array of the songs that was added.
     def addToLoved(other)
       authorize
-
+      
       songsAdded = []
 
-      other.each { |song| songsAdded << song if @lastfm.track.love(track: song.name, artist: song.artist) }
+      if @lastfm.session
+        other.each { |song| songsAdded << song if @lastfm.track.love(track: song.name, artist: song.artist) }
+      end
 
       songsAdded
     end
 
+    alias_method :addToFavorites, :addToLoved
+
     # Public: Searches for loved candidates at Last.fm.
     #
+    #
     # other         - SongSet to search for.
+    # limit         - Maximum limit for search results (default:
+    #                 @limit).
     # strict_search - True if search should be strict (default: true).
     #
     # Returns an array of loved candidates.
-    def getLovedCandidates(other, limit, strict_search = true)
+    def getLovedCandidates(other, limit = @limit, strict_search = true)
       candidates = []
 
       # Search for songs that are not already favorites and add them
@@ -85,18 +101,19 @@ module SyncSongs
       candidates
     end
 
+    alias_method :getFavoriteCandidates, :getLovedCandidates
+
     private
 
     # Internal: Authorize a Last.fm session (needed for certain calls
-    # to Last.fm) asking the user to authorize and open a page in the
-    # web browser in which the user can do this.
+    # to Last.fm). Sould be in UI?
     def authorize
       # Store token somewhere instead and only call URL if there is no
       # stored token.
       token = @lastfm.auth.get_token
+
       Launchy.open("http://www.last.fm/api/auth/?api_key=#@api_key&token=#{token}")
-      print 'A page asking for authorization of this tool with Last.fm should be open in your web browser. You need to approve it before proceeding. Continue? (y/n) '
-      exit unless gets.strip.casecmp('y') == 0
+      exit unless ask('A page asking for authorization of this tool with Last.fm should be open in your web browser. You need to approve it before proceeding. Continue? (y/n) ').casecmp('y') == 0
 
       @lastfm.session = @lastfm.auth.get_session(token: token)['key']
     end
