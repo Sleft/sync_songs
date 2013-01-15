f9e4a4ea6d2ac7aca8df40b21fd3a42233ed20d1
Sleft <fte08eas@student.lu.se>
2013-01-15 13:08:40 +0100

    Do not require rubygems http://www.rubyinside.com/why-using-require-rubygems-is-wrong-1478.html

--- lib/sync-songs/grooveshark_list.rb
+++ lib/sync-songs/grooveshark_list.rb
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-require 'rubygems'
 require 'grooveshark'
 require_relative 'song_list'
 
--- lib/sync-songs/lastfm_list.rb
+++ lib/sync-songs/lastfm_list.rb
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-require 'rubygems'
 require 'lastfm'
 require 'launchy'
 require_relative 'song_list'
