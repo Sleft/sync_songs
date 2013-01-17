1591a9ac5d62054cd9e289532d144c4a869e9da4
Sleft <fte08eas@student.lu.se>
2013-01-17 20:54:28 +0100

    More concise check for services and type. Still needs lots more work!

--- lib/sync-songs/controller.rb
+++ lib/sync-songs/controller.rb
@@ -10,32 +10,43 @@ module SyncSongs
     # Public: Constructs a controller.
     #
     # action   - A Symbol naming the action to perform.
-    # types    - A hash of services associated with types.
+    # services - A hash of services associated with types.
     # ui       - The user interface to use.
-    def initialize(action, services, ui)
+    def initialize(action, input_services, ui)
       @action = action
-      @services = services
       @ui = ui
       ui.fail('You must supply at least two distinct services.') if @services.size < 2
 
-      @directions = ui.directions(@services) if @action == :sync
+      @input_directions = ui.directions(input_services) if @action == :sync
 
       supported_services = Controller.services
-
-      # Check if service and type exists
+      
       if @action == :sync
-        @directions.each do |type, support|
-          ui.fail("#{type} is not supported.") unless supported_services.key?(type)
-          unless supported_services[type].find { |k, v| {k => v} == support } ||
-              (supported_services[type] && support == :rw)
-            ui.fail("#{support.values.join(', ')} to #{support.keys.join(', ')} for #{type} is not supported.")
-          end
+        # Check if service, type and action are supported
+        @input_directions.each do |input_service, input_support|
+          fail_msg = " is not supported."
+          input_type = input_support.keys.first
+          input_action = input_support.values.first
+
+          # Is service supported?
+          fail_msg = "#{input_service}#{fail_msg}"
+          ui.fail(fail_msg) unless supported_services.key?(input_service)
+
+          # Is type supported?
+          p supported_types = supported_services[input_service]
+          fail_msg = "#{input_type} for #{fail_msg}"
+          ui.fail(fail_msg) unless supported_types.key?(input_type)
+
+          # Is action supported?
+          fail_msg = "#{input_action} to #{fail_msg}"
+          supported_action = supported_types[input_type]
+          ui.fail(fail_msg) unless supported_action == input_action || supported_action == :rw
         end
       elsif @action == :diff
-        # Check if types of services has :r or :rw.
+        ui.fail(fail_msg) unless supported_action == :r || supported_action == :rw
       end
-
-      @services.each { |s, _| initializeUI(s) }
+      
+      input_services.each { |s, _| initializeUI(s) }
 
       @sets = []
 
@@ -48,40 +59,39 @@ module SyncSongs
       # Ask for each missing song if it shall be synced (y/n)
       # Threads (if both directions): Set data
 
+    end
+  end
 
+  # Public: Returns a hash of services associated with types of
+  # services and their support direction.
+  def self.services
+    services = {}
+
+    # Get the classes that extends SongSet.
+    classes = ObjectSpace.each_object(Class).select { |klass| klass < SongSet }
+
+    # Associate the class name with it services.
+    classes.each do |klass|
+      class_name = klass.name.split('::').last.sub(/Set\Z/, '').downcase
+      # Only accept classes that ends with 'Set'.
+      services[class_name.to_sym] = klass::SERVICES unless class_name.empty?
     end
 
-    # Public: Returns a hash of services associated with types of
-    # services and their support direction.
-    def self.services
-      services = {}
+    services
+  end
 
-      # Get the classes that extends SongSet.
-      classes = ObjectSpace.each_object(Class).select { |klass| klass < SongSet }
+  private
 
-      # Associate the class name with it services.
-      classes.each do |klass|
-        class_name = klass.name.split('::').last.sub(/Set\Z/, '').downcase
-        # Only accept classes that ends with 'Set'.
-        services[class_name.to_sym] = klass::SERVICES unless class_name.empty?
-      end
-
-      services
-    end
-
-    private
-
-    # Internal: Try to initialize the UI for the given service. Sends
-    # a message to the UI if it fails.
-    #
-    # service - A String naming the service.
-    def initializeUI(service)
-      service_ui = "#{service.capitalize}#{@ui.class.name.split('::').last}"
-      begin                     # AVOID EVAL
-        @sets << (eval "#{service_ui}.new").set
-      rescue NameError => e
-        @ui.fail("Failed to initialize #{service_ui}.", e)
-      end
+  # Internal: Try to initialize the UI for the given service. Sends
+  # a message to the UI if it fails.
+  #
+  # service - A String naming the service.
+  def initializeUI(service)
+    service_ui = "#{service.capitalize}#{@ui.class.name.split('::').last}"
+    begin                     # AVOID EVAL
+      @sets << (eval "#{service_ui}.new").set
+    rescue NameError => e
+      @ui.fail("Failed to initialize #{service_ui}.", e)
     end
   end
 end
