5bb8ae8f6bf8baa6f7e40803ddd8379a64fea526
Sleft <fte08eas@student.lu.se>
2013-01-18 17:55:26 +0100

    Rescue Grooveshark login failure

--- lib/sync_songs/grooveshark_cli.rb
+++ lib/sync_songs/grooveshark_cli.rb
@@ -10,8 +10,23 @@ module SyncSongs
     attr_reader :set
 
     def initialize
+      @logged_in = false
+
+      until @logged_in
+        tryLogin
+      end
+    end
+
+    private
+
+    # Internal: Tries to login to Grooveshark and prints and error
+    # message if it fails.
+    def tryLogin
       @set = GroovesharkSet.new(ask('Grooveshark username? '),
                                 ask('Grooveshark password? ') { |q| q.echo = false })
+      @logged_in = true
+    rescue Grooveshark::InvalidAuthentication => e
+      say e.message
     end
   end
 end
