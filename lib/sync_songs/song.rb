88def089a7c6102b533ac6faf674cb1f5a237507
Sleft <fte08eas@student.lu.se>
2013-03-02 22:12:34 +0100

    Fixed documentation of attributes

--- lib/sync_songs/song.rb
+++ lib/sync_songs/song.rb
@@ -13,8 +13,12 @@ module SyncSongs
     # Public: Constructs a song. Leading and trailing whitespace is
     # removed as it has no semantic significance for songs.
     #
-    # name   - The name of the song.
-    # artist - The artist performing the song.
+    # name     - The name of the song.
+    # artist   - The artist performing the song.
+    # album    - The album the song is found on (default: nil).
+    # duration - The duration of the song in seconds (default: nil).
+    # id       - An id of for the song (default: nil). Is service
+    #            relative.
     #
     # Raises ArgumentError if the artist or the name is empty.
     def initialize(name, artist, album = nil, duration = nil, id = nil)
