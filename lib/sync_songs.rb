47563e9cebce987a24ce73cdf36f405b8b29b73a
Sleft <fte08eas@student.lu.se>
2013-03-17 13:35:55 +0100

    More docs

--- lib/sync_songs.rb
+++ lib/sync_songs.rb
@@ -3,19 +3,24 @@
 require 'rubygems'
 require 'bundler/setup'
 
-# Path to the library
 PATH = './sync_songs/'
 SERVICES_PATH = "#{PATH}services/"
 
-# Load the library
+# Load the library.
 require_relative "#{PATH}cli"
 require_relative "#{PATH}controller"
 require_relative "#{PATH}version"
 
 require_relative "#{SERVICES_PATH}service_controller"
+
+# Load sub classes of ServiceController.
 require_relative "#{SERVICES_PATH}csv_controller"
 require_relative "#{SERVICES_PATH}grooveshark_controller"
 require_relative "#{SERVICES_PATH}lastfm_controller"
 
 # Internal: A sync direction.
-Struct.new('Direction', :services, :direction)
+Struct.new('Direction',
+           # Internal: An array of two services to sync between.
+           :services,
+           # Internal: The direction to sync in.
+           :direction)
